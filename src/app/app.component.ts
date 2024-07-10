1937412448c6ea4a5144d33538b6d34fd863d7cb
Zoey Guo <zoey-guo@uiowa.edu>
2024-07-10 15:54:04 -0500

    added nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomepageComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, HomepageComponent],
+  imports: [RouterOutlet, HomeComponent],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
