9d2dcbdba540848cb03c97645ade0104f0c83e92
Zoey Guo <zoey-guo@uiowa.edu>
2024-07-10 14:23:41 -0500

    added home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomepageComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomepageComponent],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
